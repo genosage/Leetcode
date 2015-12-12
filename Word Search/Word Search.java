716c4e6b46b57499eec4fb7f76f5e99dd7581aaa
genosage <zhangtian1104@gmail.com>
2015-12-12 18:13:39 +1100

    79
    Code refactoring

--- Word Search/Word Search.java
+++ Word Search/Word Search.java
@@ -1,70 +1,58 @@
 public class Solution {
-    public boolean findNear(char[][] board, int[][] markUsed, char[] wordArray, int i, int j, int target){
-        markUsed[i][j] = 1;
-        if(i > 0 && markUsed[i - 1][j] == 0 && board[i - 1][j] == wordArray[target]){
-            if(target == wordArray.length - 1){
-                return true;
-            }else{
-                if(findNear(board, markUsed, wordArray, i - 1, j, target + 1)){
-                    return true;
-                }
-                markUsed[i - 1][j] = 0;
-            }
+    public boolean match(char[][] board, int[][] markUsed, char[] wordArray, int i, int j, int target) {
+
+        if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1) {
+            return false;
         }
-        if(i < board.length - 1 && markUsed[i + 1][j] == 0 && board[i + 1][j] == wordArray[target]){
-            if(target == wordArray.length - 1){
+
+        if (markUsed[i][j] == 0 && board[i][j] == wordArray[target]) {
+
+            if (target == wordArray.length - 1) {
                 return true;
-            }else{
-                if(findNear(board, markUsed, wordArray, i + 1, j, target + 1)){
-                    return true;
-                }
-                markUsed[i + 1][j] = 0;
             }
-        }
-        if(j > 0 && markUsed[i][j - 1] == 0 && board[i][j - 1] == wordArray[target]){
-            if(target == wordArray.length - 1){
+
+            markUsed[i][j] = 1;
+
+            if (match(board, markUsed, wordArray, i - 1, j, target + 1)) {
                 return true;
-            }else{
-                if(findNear(board, markUsed, wordArray, i, j - 1, target + 1)){
-                    return true;
-                }
-                markUsed[i][j - 1] = 0;
             }
-        }
-        if(j < board[i].length - 1 && markUsed[i][j + 1] == 0 && board[i][j + 1] == wordArray[target]){
-            if(target == wordArray.length - 1){
+            if (match(board, markUsed, wordArray, i + 1, j, target + 1)) {
                 return true;
-            }else{
-                if(findNear(board, markUsed, wordArray, i, j + 1, target + 1)){
-                    return true;
-                }
-                markUsed[i][j + 1] = 0;
             }
+            if (match(board, markUsed, wordArray, i, j - 1, target + 1)) {
+                return true;
+            }
+            if (match(board, markUsed, wordArray, i, j + 1, target + 1)) {
+                return true;
+            }
+            
+            markUsed[i][j] = 0;
         }
         return false;
     }
+
     public boolean exist(char[][] board, String word) {
         char[] wordArray = word.toCharArray();
         int[][] markUsed = new int[board.length][board[0].length];
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
                 markUsed[i][j] = 0;
             }
         }
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if(wordArray.length == 1){
-                    if (board[i][j] == wordArray[0]){
+
+        if (board == null) {
+            return false;
+        }
+        if (word == null || word.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                    if (match(board, markUsed, wordArray, i, j, 0)) {
                         return true;
                     }
-                }else if(wordArray.length > 1){
-                    if (board[i][j] == wordArray[0]){
-                        if(findNear(board, markUsed, wordArray, i, j, 1)){
-                            return true;
-                        }
-                        markUsed[i][j] = 0;
-                    }
-                }
             }
         }
         return false;
