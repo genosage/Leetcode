b2c496489decb1f09a6927655d961b9526a4ab36
genosage <zhangtian1104@gmail.com>
2015-12-17 20:04:16 +1100

    35

---
+++ Search Insert Position/Search Insert Position.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums.length == 0 || nums == null){
+            return 0;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(target <= nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
