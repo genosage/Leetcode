4722df9907ca1711edb03acbe366c29f79de27d3
genosage <zhangtian1104@gmail.com>
2015-12-17 18:45:30 +1100

    169

--- Majority Element/Majority Element.java
+++ Majority Element/Majority Element.java
@@ -7,14 +7,11 @@ public class Solution {
             }else{
                 map.put(nums[i], 1);
             }
-        }
-        
-        for(int i = 0; i < nums.length; i++){
+            
             if(map.get(nums[i]) > nums.length / 2){
                 return nums[i];
             }
         }
-        
         return 0;
     }
 }
