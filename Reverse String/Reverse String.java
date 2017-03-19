85f166b8149f5f4224576aad2ffa579fbfd211f4
Josh Zhang <tianz2@student.unimelb.edu.au>
2017-03-19 17:07:31 +1100

    344

---
+++ Reverse String/Reverse String.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public String reverseString(String s) {
+        if (s == null || s == "") return "";
+        
+        char[] charArr = s.toCharArray();
+        
+        int i = 0;
+        int j = charArr.length - 1;
+        
+        while(i < j) {
+            char temp = charArr[i];
+            charArr[i] = charArr[j];
+            charArr[j] = temp;
+            
+            i++;
+            j--;
+        }
+        
+        return new String(charArr);
+    }
+}
